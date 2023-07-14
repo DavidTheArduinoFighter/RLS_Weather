c4523b16a07a4c6d4b267d249e34a8cf7c615e4b
Ogorevc <david.ogorevc@rls.si>
2023-07-14 12:19:38 +0200

    Class for data added

---
+++ Get_Data.py
@@ -0,0 +1,14 @@
+
+
+class Data:
+    def __init__(self):
+        pass
+
+    def temperature(self):
+        return 0
+
+    def wind(self):
+        return 0
+
+    def humidity(self):
+        return 0
