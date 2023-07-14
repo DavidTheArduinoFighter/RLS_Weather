c8b5fe401c658a40bb0d6e99b7d007812a79f7d3
Ogorevc <david.ogorevc@rls.si>
2023-07-14 12:05:33 +0200

    UI Handler class

--- PythonUiHandler.py
+++ PythonUiHandler.py
@@ -3,20 +3,31 @@ from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import QFile, QIODevice
 
+
+class ShowApp:
+    def __init__(self, ui_file_name):
+        self.app = QApplication(sys.argv)
+        self.ui_file = QFile(ui_file_name)
+        self.ui_file_name = ui_file_name
+        self.open()
+        self.run()
+
+    def open(self):
+        if not self.ui_file.open(QIODevice.ReadOnly):
+            print(f"Cannot open {self.ui_file_name}: {self.ui_file.errorString()}")
+            sys.exit(-1)
+
+    def run(self):
+        loader = QUiLoader()
+        window = loader.load(self.ui_file)
+        self.ui_file.close()
+        if not window:
+            print(loader.errorString())
+            sys.exit(-1)
+        window.show()
+        sys.exit(self.app.exec())
+
+
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-
-    ui_file_name = "GUI.ui"
-    ui_file = QFile(ui_file_name)
-    if not ui_file.open(QIODevice.ReadOnly):
-        print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
-        sys.exit(-1)
-    loader = QUiLoader()
-    window = loader.load(ui_file)
-    ui_file.close()
-    if not window:
-        print(loader.errorString())
-        sys.exit(-1)
-    window.show()
-
-    sys.exit(app.exec())
+    ui = "GUI.ui"
+    App = ShowApp(ui)
