55270a3940b2efb8375ff812c5c39bbe4c2c7bd4
DavidTheArduinoFighter <65223452+DavidTheArduinoFighter@users.noreply.github.com>
2023-09-11 06:24:53 +0200

    Update main.py
    Update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import sys
+import Get_Data
 from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox, QPushButton
 from PyQt5 import QtCore
 from GUI import Ui_MainWindow
 import PythonUiHandler as UI
-import sys
-import Get_Data
 
 
 class Window(QMainWindow):
